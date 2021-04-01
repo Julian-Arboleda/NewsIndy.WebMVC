d62b81b5c822c6974ec765856cc04ec61afea77d
Julian <arboledajulian@outlook.com>
2021-04-01 12:57:04 -0400

    creating classes

---
+++ NewsIndy.Data/Organization.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsIndy.Data
+{
+   public class Organization
+    {
+        [Key]
+        public int OrgId { get; set; }
+        public string Name { get; set; }
+        public bool IsFoodBank { get; set; }
+
+        [ForeignKey(nameof(Borough))]
+        public int BoroughId { get; set; }
+        public virtual Borough Borough { get; set; }
+    }
+}
