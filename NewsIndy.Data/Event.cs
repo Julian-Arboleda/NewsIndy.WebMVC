1aa81473d84dee3cc6c5725db41f990c6bec1259
Jay A <jay@localhost.localdomain>
2021-04-07 03:35:21 -0400

    lolol ignore this bullshit

---
+++ NewsIndy.Data/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Annotations;
+
+namespace NewsIndy.Data
+{
+    public class Event
+    {
+        [Key]
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public string Description {get;set;}
+        
+    }
+}
